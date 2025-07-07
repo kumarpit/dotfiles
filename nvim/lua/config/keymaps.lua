d8f31978e2bfea0c989ce0622c8fd3aae547ad5a
Arpit Kumar <kumar.arpit77@yahoo.com>
2025-07-07 08:10:07 -0700

    Update keymaps.lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,8 +2,46 @@
 vim.keymap.set(
     "n",
     "<leader>cp",
-    ":AsyncRun latexmk -pvc -pdf -outdir=build %<CR>",
+    ":AsyncRun latexmk -pvc -pdf -outdir=./build %<CR>",
     { desc = "Compile latex and watch for changes" }
 )
-vim.keymap.set("n", "<leader>bb", ":GitBlameToggle <CR>", { desc = "Toggle Git blame" })
-vim.keymap.set("n", "<leader>tt", ":TodoTelescope <CR>", { desc = "Search TODOs" })
+vim.keymap.set(
+    "n",
+    "<leader>bb",
+    ":GitBlameToggle <CR>",
+    { desc = "Toggle Git blame" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>tt",
+    ":TodoTelescope <CR>",
+    { desc = "Search TODOs" }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>xn",
+    ":lua vim.diagnostic.goto_next() <CR>",
+    { desc = "Go to next error/warning" }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>xp",
+    ":lua vim.diagnostic.goto_prev() <CR>",
+    { desc = "Go to next error/warning" }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>rr",
+    ":lua vim.lsp.buf.rename() <CR>",
+    { desc = "Rename symbol and update occurrences" }
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>gu",
+    ":lua vim.lsp.buf.references() <CR>",
+    { desc = "List all references" }
+)
