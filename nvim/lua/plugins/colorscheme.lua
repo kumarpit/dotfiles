dfb5654c94db693cfe08ff2436f614979761d2d1
Arpit Kumar <kumar.arpit77@yahoo.com>
2025-07-24 12:29:49 -0700

    embracing gruvbox

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,8 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        opts = {
-            transparent = true,
-            styles = {
-                sidebars = "transparent",
-                floats = "transparent",
-            },
+        {
+                "ellisonleao/gruvbox.nvim",
+                priority = 1000,
+                config = true,
         },
-    },
 }
+
