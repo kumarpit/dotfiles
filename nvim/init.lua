b3abe73a0ad6126a1be9b25aef1ba6f648f6a933
Arpit Kumar <kumar.arpit77@yahoo.com>
2025-07-24 12:30:28 -0700

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.o.background = "dark" -- or "light" for light mode
+require("gruvbox").setup({
+        transparent_mode = true,
+})
+vim.cmd("colorscheme gruvbox")
+vim.cmd([[colorscheme gruvbox]])
+
+require("oil").setup()
